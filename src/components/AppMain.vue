10828e6a0c6c25c767b64f1b250993737f9f7e85
GiacomoBozzolino <g.bozzolino@gmail.com>
2023-06-20 16:42:42 +0200

    [FEAT]lavoro seconda sezione main

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,5 +1,31 @@
 <script>
     export default {
+        data() {
+            return {
+                menu: [
+                    {
+                        label: 'DIGITAL COMICS',
+                        img:'buy-comics-digital-comics.png',
+                    },
+                    {
+                        label: 'DC MERCHANDISE',
+                        img:'../assets/img/buy-comics-merchandise.png',
+                    },
+                    {
+                        label: 'SUBSCRIPTION',
+                        img:'../assets/img/buy-comics-subscription.png',
+                    },
+                    {
+                        label: 'COMIC SHOP LOCATOR',
+                        img:'../assets/img/buy-comics-shop-locator.png',
+                    },
+                    {
+                        label: 'DC POWER VISA',
+                        img :'../assets/img/buy-dc-power-visa.svg',
+                    },
+                ]
+            }
+        },
         
     }
 </script>
@@ -8,6 +34,16 @@
     <div class="container">
         <div class="content">
             <div> --Content goes here-- </div> 
+            
+        </div>
+        <div class="blue-content">
+            <ul>
+                <li v-for="(item, index) in menu" :key="index">
+                    <img :src="item.img" alt="">
+                    <div>{{item.label}}</div>
+                </li>
+                
+            </ul>
         </div>
 
     </div>
@@ -25,6 +61,16 @@
             font-size: 40px;
             padding: 0 20px; 
         }
+}
+
+
+.blue-content{
+    height: 200px;
+    background-color:#1F81FA ;
+        ul{
+            display: flex;
+        }
+
 }
     
 </style>
